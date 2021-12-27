72e3f808201fa962b888512fee8a7466d7f13291
sekai5108 <sekai.5108@gmail.com>
2021-12-27 04:40:56 +0000

    Add users/showの表示部分にcommunity投稿部分を追加

--- app/controllers/user/communities_controller.rb
+++ app/controllers/user/communities_controller.rb
@@ -1,5 +1,5 @@
 class User::CommunitiesController < ApplicationController
-  before_action :authenticate_user!, except:[:index]
+  before_action :authenticate_user!, except:[:index, :show]
 
   def new
     @community = Community.new
