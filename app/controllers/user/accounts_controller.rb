d4dffab7cd782bc5184171f22f3c4aeb4e44a4bc
sekai5108 <sekai.5108@gmail.com>
2021-12-27 11:47:37 +0000

    修正

--- app/controllers/user/accounts_controller.rb
+++ app/controllers/user/accounts_controller.rb
@@ -31,6 +31,7 @@ class User::AccountsController < ApplicationController
     redirect_to accounts_path
   end
 
+
   private
   def account_params
     params.require(:account).permit(:account_name)
